209dc936b96ce4c58413b163b039f0679c985931
QDoussot <quentin.doussot@gmail.com>
2024-12-03 00:31:33 +0100

    Finally add day2

---
+++ src/day2/damper.rs
@@ -0,0 +1,65 @@
+use aoc_runner_derive::aoc;
+use itertools::Itertools;
+
+use crate::day2::are_safe_adjacent;
+use crate::day2::Validation;
+use crate::day2::{parse_reports, PlantReport};
+
+struct Damped<'a>(&'a PlantReport, usize);
+
+//TODO Eventually, homogeneize interface with PlantReport with a Trait
+impl<'a> Damped<'a> {
+    pub fn iter(&'a self) -> impl Iterator<Item = usize> + 'a {
+        self.0
+             .0
+            .iter()
+            .enumerate()
+            .filter_map(|(i, val)| (i != self.1).then_some(*val))
+    }
+
+    fn has_safe_adjacent_levels(&self) -> bool {
+        self.iter()
+            .tuple_windows()
+            .all(|(w0, w1)| are_safe_adjacent(w0, w1))
+    }
+
+    fn is_safe_pred(&self, predicat: fn(usize, usize) -> bool) -> bool {
+        self.iter().tuple_windows().all(|(w0, w1)| predicat(w0, w1))
+            && self.has_safe_adjacent_levels()
+    }
+
+    pub fn every_possible_dampening(report:&'a PlantReport) -> impl Iterator<Item = Damped<'a>> {
+        (0..report.0.len()).map(move |damped| Damped(&report, damped))
+    }
+
+}
+
+fn find_bad_and_damper(
+    report: &PlantReport,
+    predicat: fn(usize, usize) -> bool,
+) -> Option<Validation> {
+    match report
+        .0
+        .windows(2)
+        .find_position(|w| !(predicat(w[0], w[1]) && are_safe_adjacent(w[0], w[1])))
+    {
+        None => Some(Validation::Safe),
+        Some((pos, _)) => Damped(&report, pos)
+            .is_safe_pred(predicat)
+            .then_some(Validation::Damping(pos))
+            .or_else(|| {
+                Damped(&report, pos + 1)
+                    .is_safe_pred(predicat)
+                    .then_some(Validation::Damping(pos + 1))
+            }),
+    }
+}
+#[aoc(day2, part2)]
+fn number_of_safe_reports_with_dampener_bis(reports: &Vec<PlantReport>) -> usize {
+    reports
+        .iter()
+        .filter(|pr| find_bad_and_damper(&pr, |w0, w1| w0 < w1).is_some()
+            || find_bad_and_damper(&pr, |w0, w1| w0 > w1).is_some()
+            )
+        .count()
+}
---
+++ src/day2/mod.rs
@@ -0,0 +1,50 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use derive_more::From;
+use eyre::Report;
+
+use eyre::Error;
+use huparse::parse::ParserMapper;
+use huparse::parse::{Parsable, Parse};
+use huparse::parser;
+
+#[derive(From)]
+pub struct PlantReport(pub Vec<usize>);
+
+pub fn are_safe_adjacent(l0: usize, l1: usize) -> bool {
+    (1..4).contains(&l0.abs_diff(l1))
+}
+
+impl Parsable for PlantReport {
+    fn get_parser() -> impl huparse::parse::Parser<Self> {
+        ParserMapper::new(parser!([usize | " "]))
+    }
+}
+
+impl PlantReport {
+    fn is_safe(&self) -> bool {
+        (self.0.windows(2).all(|w| w[0] < w[1]) || self.0.windows(2).all(|w| w[0] > w[1]))
+            && self.0.windows(2).all(|w| are_safe_adjacent(w[0], w[1]))
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Validation {
+    Safe,
+    Damping(usize),
+}
+
+#[aoc_generator(day2)]
+fn parse_reports(input: &str) -> Result<Vec<PlantReport>, Report> {
+    parser!([{ PlantReport } | "\n"]).parse_top(input)
+}
+
+#[aoc(day2, part1)]
+fn number_of_safe_reports(reports: &Vec<PlantReport>) -> usize {
+    reports
+        .iter()
+        .filter(|report| report.is_safe())
+        .count()
+}
+
+
+mod damper;
