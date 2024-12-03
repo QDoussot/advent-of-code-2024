caaa4986c27929342a9c1bdfe951b66e2d93e4ce
QDoussot <quentin.doussot@gmail.com>
2024-12-03 14:05:17 +0100

    Tidy up day2 implementation

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -6,12 +6,10 @@ use huparse::parse::ParserMapper;
 use huparse::parse::{Parsable, Parse};
 use huparse::parser;
 
+use itertools::Itertools;
+
 #[derive(From)]
 pub struct PlantReport(pub Vec<usize>);
-
-pub fn are_safe_adjacent(l0: usize, l1: usize) -> bool {
-    (1..4).contains(&l0.abs_diff(l1))
-}
 
 impl Parsable for PlantReport {
     fn get_parser() -> impl huparse::parse::Parser<Self> {
@@ -19,17 +17,48 @@ impl Parsable for PlantReport {
     }
 }
 
-impl PlantReport {
-    fn is_safe(&self) -> bool {
-        (self.0.windows(2).all(|w| w[0] < w[1]) || self.0.windows(2).all(|w| w[0] > w[1]))
-            && self.0.windows(2).all(|w| are_safe_adjacent(w[0], w[1]))
+trait ReportInterpretation {
+    fn levels<'a>(&'a self) -> impl 'a + Iterator<Item = &'a usize>;
+}
+
+type Predicate = fn((&usize, &usize)) -> bool;
+static INCREASING: Predicate = |(a, b)| a < b;
+static DECREASING: Predicate = |(a, b)| a > b;
+static NEAR: Predicate = |(a, b)| (1..4).contains(&a.abs_diff(*b));
+
+macro_rules! and {
+    ($p1:ident,$p2:ident) => {
+        |(a, b)| $p1((a, b)) && $p2((a, b))
+    };
+}
+macro_rules! not {
+    ($p1:ident) => {
+        |(a, b)| !$p1((a, b))
+    };
+}
+
+struct Raw<'a>(&'a PlantReport);
+
+impl<'a> ReportInterpretation for Raw<'a> {
+    fn levels<'b>(&'b self) -> impl 'b + Iterator<Item = &'b usize> {
+        self.0 .0.iter()
     }
 }
 
-#[derive(PartialEq, Debug)]
-pub enum Validation {
-    Safe,
-    Damping(usize),
+trait ReportInterpretationEx: ReportInterpretation {
+    fn check_consecutive(&self, predicate: Predicate) -> bool;
+    fn is_safe(&self) -> bool;
+}
+
+impl<T: ReportInterpretation> ReportInterpretationEx for T {
+    fn check_consecutive(&self, predicate: Predicate) -> bool {
+        self.levels().tuple_windows().all(predicate)
+    }
+
+    fn is_safe(&self) -> bool {
+        (self.check_consecutive(INCREASING) || self.check_consecutive(DECREASING))
+            && self.check_consecutive(NEAR)
+    }
 }
 
 #[aoc_generator(day2)]
@@ -39,7 +68,51 @@ fn parse_reports(input: &str) -> Result<Vec<PlantReport>, Report> {
 
 #[aoc(day2, part1)]
 fn number_of_safe_reports(reports: &Vec<PlantReport>) -> usize {
-    reports.iter().filter(|report| report.is_safe()).count()
+    reports
+        .iter()
+        .filter(|report| Raw(report).is_safe())
+        .count()
 }
 
-mod damper;
+#[derive(PartialEq, Debug)]
+pub enum Validation {
+    Safe,
+    Damping(usize),
+}
+
+struct Damped<'a>(&'a PlantReport, usize);
+
+impl<'a> ReportInterpretation for Damped<'a> {
+    fn levels<'b>(&'b self) -> impl 'b + Iterator<Item = &'b usize> {
+        self.0
+             .0
+            .iter()
+            .enumerate()
+            .filter_map(|(i, val)| (i != self.1).then_some(val))
+    }
+}
+
+fn try_validate(damped: Damped, predicate: Predicate) -> Option<Validation> {
+    damped
+        .check_consecutive(predicate)
+        .then_some(Validation::Damping(damped.1))
+}
+
+fn find_bad_and_damper(report: &PlantReport, predicate: Predicate) -> Option<Validation> {
+    match report.0.iter().tuple_windows().find_position(not!(predicate)) {
+        None => Some(Validation::Safe),
+        Some((pos, _)) => try_validate(Damped(&report, pos), predicate)
+            .or_else(|| try_validate(Damped(&report, pos + 1), predicate)),
+    }
+}
+
+#[aoc(day2, part2)]
+fn number_of_dampened_safe_reports(reports: &Vec<PlantReport>) -> usize {
+    reports
+        .iter()
+        .filter(|report| {
+            find_bad_and_damper(&report, and!(INCREASING, NEAR)).is_some()
+                || find_bad_and_damper(&report, and!(DECREASING, NEAR)).is_some()
+        })
+        .count()
+}
