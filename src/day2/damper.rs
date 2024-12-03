6bda71be6a9dc623cafd770b990a98410abb2c6a
QDoussot <quentin.doussot@gmail.com>
2024-12-03 14:03:36 +0100

    Commit before delete

--- src/day2/damper.rs
+++ src/day2/damper.rs
@@ -3,9 +3,9 @@ use itertools::Itertools;
 
 use crate::day2::are_safe_adjacent;
 use crate::day2::Validation;
-use crate::day2::{parse_reports, PlantReport};
+use crate::day2::{parse_reports, RawReport};
 
-struct Damped<'a>(&'a PlantReport, usize);
+struct Damped<'a>(&'a RawReport, usize);
 
 //TODO Eventually, homogeneize interface with PlantReport with a Trait
 impl<'a> Damped<'a> {
@@ -23,14 +23,15 @@ impl<'a> Damped<'a> {
             .all(|(w0, w1)| are_safe_adjacent(w0, w1))
     }
 
-    fn is_safe_pred(&self, predicat: fn(usize, usize) -> bool) -> bool {
-        self.iter().tuple_windows().all(|(w0, w1)| predicat(w0, w1))
-            && self.has_safe_adjacent_levels()
+    fn try_validate(&self, predicat: fn(usize, usize) -> bool) -> Option<Validation> {
+        (self.iter().tuple_windows().all(|(w0, w1)| predicat(w0, w1))
+            && self.has_safe_adjacent_levels())
+        .then_some(Validation::Damping(self.1))
     }
 }
 
 fn find_bad_and_damper(
-    report: &PlantReport,
+    report: &RawReport,
     predicat: fn(usize, usize) -> bool,
 ) -> Option<Validation> {
     match report
@@ -40,17 +41,12 @@ fn find_bad_and_damper(
     {
         None => Some(Validation::Safe),
         Some((pos, _)) => Damped(&report, pos)
-            .is_safe_pred(predicat)
-            .then_some(Validation::Damping(pos))
-            .or_else(|| {
-                Damped(&report, pos + 1)
-                    .is_safe_pred(predicat)
-                    .then_some(Validation::Damping(pos + 1))
-            }),
+            .try_validate(predicat)
+            .or_else(|| Damped(&report, pos + 1).try_validate(predicat)),
     }
 }
 #[aoc(day2, part2)]
-fn number_of_safe_reports_with_dampener_bis(reports: &Vec<PlantReport>) -> usize {
+fn number_of_safe_reports_with_dampener_bis(reports: &Vec<RawReport>) -> usize {
     reports
         .iter()
         .filter(|pr| {
