549d7fd1b254ecb91d74541e10700aaf1e2def0a
QDoussot <quentin.doussot@gmail.com>
2024-12-07 23:31:14 +0100

    Add template file for day 8

---
+++ src/day08.rs
@@ -0,0 +1,23 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use eyre::Report;
+
+use huparse::parse::Parse;
+use huparse::parser;
+
+type ParsedInput = ();
+
+#[aoc_generator(day8)]
+fn parse_day8(input: &str) -> Result<ParsedInput, Report> {
+    Ok(())
+}
+
+#[aoc(day8, part1)]
+fn solve_part1(input: &ParsedInput) -> Result<usize, String> {
+    Ok(0)
+}
+
+#[aoc(day8, part2)]
+fn solve_part2(input: &ParsedInput) -> Result<usize, String> {
+    Ok(0)
+}
+
