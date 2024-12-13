1b9120e26a1f79925ff818f28802a6d1460fa743
QDoussot <quentin.doussot@gmail.com>
2024-12-13 20:52:29 +0100

    [Day 13] Refactor to have a more standalone, shorter code

--- src/day13.rs
+++ src/day13.rs
@@ -1,108 +1,53 @@
-use derive_more::Mul;
-
 use aoc_runner_derive::{aoc, aoc_generator};
-use derive_more::Add;
-use derive_more::Sub;
-use eyre::Report;
 use itertools::Itertools;
 
-use huparse::parse::Parse;
-use huparse::parser;
-
-#[derive(Debug, Copy, Clone, Add, Sub, Hash, PartialEq, Eq, Mul)]
-struct Position(i64, i64);
-
-#[derive(Debug, Copy, Clone, Add, Sub, Hash, PartialEq, Eq, Mul)]
-struct Play {
-    a: Position,
-    b: Position,
-    p: Position,
-}
-
-type ParsedInput = Vec<Play>;
+type Play = (i64, i64, i64, i64, i64, i64);
 
 #[aoc_generator(day13)]
-fn parse_day13(input: &str) -> Result<ParsedInput, Report> {
-    let parser = parser!([(
-        "\
-Button A: X+%, Y+%
-Button B: X%, Y+%
-Prize: X=%, Y=%",
-        i64,
-        i64,
-        i64,
-        i64,
-        i64,
-        i64
-    ) | "\n\n"]);
-
-    let res = parser.parse_top(&input)?;
-    let res = res
+fn parse_day13(input: &str) -> Vec<Play> {
+    input
+        .chars()
+        .chunk_by(|c| char::is_ascii_digit(c))
         .into_iter()
-        .map(|(ax, ay, bx, by, px, py)| Play {
-            a: Position(ax, ay),
-            b: Position(bx, by),
-            p: Position(px, py),
-        })
-        .collect();
-    Ok(res)
+        .filter_map(|(_, v)| v.collect::<String>().parse().ok())
+        .tuples()
+        .collect_vec()
 }
 
-fn find_token_nbr(play: &Play) -> Option<i64> {
-    // Literally follow the proposed algorithm
-    // Fast to write, quite unefficient
+// Literally follow the proposed algorithm, fast to write, quite unefficient
+fn find_token_nbr((ax, ay, bx, by, px, py): &Play) -> Option<i64> {
     (0..100)
         .cartesian_product(0..100)
-        .filter(|(an, bn)| play.a * an + play.b * bn == play.p)
+        .filter(|(an, bn)| (ax * an + bx * bn == *px) && (ay * an + by * bn == *py))
         .map(|(an, bn)| an * 3 + bn)
         .min()
 }
 
 #[aoc(day13, part1)]
-fn solve_part1(input: &ParsedInput) -> Result<i64, String> {
-    let res: i64 = input
-        .iter()
-        .map(|p| find_token_nbr(p))
-        .filter_map(|t| t)
-        .sum();
-    Ok(res)
+fn solve_part1(input: &[Play]) -> i64 {
+    input.iter().filter_map(|p| find_token_nbr(p)).sum()
 }
 
-// Solve v in au + bv = p like a regular two unknows - two equations system
-// and then check that the result is an integer
-// with
-//   a,b being integer 
-//   b,v,p being 2 dimensions integer 'vectors'
-fn compute_v(play: &Play) -> Option<i64> {
-    let Play { a, b, p } = play;
+// Solve au + bv = p like a regular two unknows - two equations system
+fn solve_equation_system((ax, ay, bx, by, px, py): &Play) -> Option<(i64, i64)> {
     let (v, rem) = {
-        let num = p.1 * a.0 - p.0 * a.1;
-        let denum = b.1 * a.0 - b.0 * a.1;
+        let num = py * ax - px * ay;
+        let denum = by * ax - bx * ay;
         (num / denum, num % denum)
     };
-    (rem == 0).then_some(v)
-}
-
-// Compute u once v is found
-fn compute_u(play: &Play, v: i64) -> Option<i64> {
-    let Play { a, b, p } = play;
-    let num = p.0 - v * b.0;
-    (num % a.0 == 0).then_some(num / a.0)
+    if rem == 0 {
+        let num = px - v * bx;
+        (num % ax == 0).then_some((num / ax, v))
+    } else {
+        None
+    }
 }
 
 #[aoc(day13, part2)]
-fn solve_part2(input: &ParsedInput) -> Result<i64, String> {
-    let res: i64 = input
+fn solve_part2(input: &[Play]) -> i64 {
+    input
         .iter()
-        .map(|p| Play {
-            a: p.a,
-            b: p.b,
-            p: p.p + Position(10000000000000, 10000000000000),
-        })
-        .filter_map(|p| Some((p, compute_v(&p)?)))
-        .filter_map(|(p, v)| Some((compute_u(&p, v)?, v)))
+        .filter_map(|play| solve_equation_system(play))
         .map(|(u, v)| 3 * u + v)
-        .sum();
-
-    Ok(res)
+        .sum()
 }
